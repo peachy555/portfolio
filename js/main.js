259ad7969c4862328bed99669174826302a697c8
Peach Tuntiwong <peach.tuntiwong@gmail.com>
2017-05-11 12:02:55 +1000

    update master => gh-pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
-  $(document).on('click', '.contacts.clickable', function(e){
-    window.open($(e.target).html(), '_blank');
+  $('.projects.content').hide();
+  $('.projects.item').hover(function() {
+    $(this).children().show();
+  }, function() {
+    $(this).children().hide();
+  })
+
+  $(document).on('click', '.clickable', function(e){
+    var link = $(e.target).attr("link");
+    window.open(link, '_blank');
   });
+
+  // $('html, body').animate({
+  //   'scrollTop': $('#anchorName2').offset().top
+  // }, 2000);
+
+//   $(document).on('click', 'a', function(event){
+//     event.preventDefault();
+//
+//     $('html, body').animate({
+//         scrollTop: $( $.attr(this, 'href') ).offset().top
+//     }, 500);
+// });
 })
